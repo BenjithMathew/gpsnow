00316e101657725c6f48c953bfca833e14107e17
aj0109 <ashrayvjoshi@gmail.com>
2016-06-11 15:28:30 +0530

    login activity working successfully!

--- app/src/main/java/com/surroundsync/gpsnow/MainActivity.java
+++ app/src/main/java/com/surroundsync/gpsnow/MainActivity.java
@@ -1,220 +1,198 @@
 package com.surroundsync.gpsnow;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.text.TextUtils;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 
-
-
-
-
-public class MainActivity extends BaseActivity implements
-        View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "EmailPassword";
+    private LocationManager locationManager;
+    private Location location;
+    private String provider;
+    private String stringLatitude = null;
+    private String stringLongitude = null;
+    private String gpsLocation =null;
 
-    private TextView mStatusTextView;
-    private TextView mDetailTextView;
-    private EditText mEmailField;
-    private EditText mPasswordField;
-    private Button email_create_account_button;
-    private Button email_sign_in_button;
+    String passwordFromServer, userNameFromServer, nameFromServer;
 
-    // [START declare_auth]
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    private Button btnSignUp;
+    boolean loginStatus = false;
+    private Criteria criteria;
+
     private FirebaseAuth mAuth;
-    // [END declare_auth]
+    private DatabaseReference mDatabase;
 
-    // [START declare_auth_listener]
     private FirebaseAuth.AuthStateListener mAuthListener;
-    // [END declare_auth_listener]
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSignUp = (Button) findViewById(R.id.activity_btn_signup_main);
+        etUsername = (EditText) findViewById(R.id.activity_username_et_login);
+        etPassword = (EditText) findViewById(R.id.activity_pass_et_login);
+        btnLogin = (Button) findViewById(R.id.activity_btn_login_ma);
 
-        // Views
-        mStatusTextView = (TextView) findViewById(R.id.status);
-        mDetailTextView = (TextView) findViewById(R.id.detail);
-        mEmailField = (EditText) findViewById(R.id.field_email);
-        mPasswordField = (EditText) findViewById(R.id.field_password);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        // Buttons
-        email_sign_in_button=(Button) findViewById(R.id.email_sign_in_button);
-        email_create_account_button=(Button) findViewById(R.id.email_create_account_button);
-
-        email_create_account_button.setOnClickListener(this);
-        email_sign_in_button.setOnClickListener(this);
-
-        // [START initialize_auth]
-        mAuth = FirebaseAuth.getInstance();
-        // [END initialize_auth]
-
-        // [START auth_state_listener]
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
+        /*btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    // User is signed out
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
-                }
-                // [START_EXCLUDE]
-                updateUI(user);
-                // [END_EXCLUDE]
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
             }
-        };
-        // [END auth_state_listener]
-    }
+        });*/
 
-    // [START on_start_add_listener]
-    @Override
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-    // [END on_start_add_listener]
+        btnLogin.setOnClickListener(new View.OnClickListener() {
 
-    // [START on_stop_remove_listener]
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
-    }
-    // [END on_stop_remove_listener]
 
-    private void createAccount(String email, String password) {
-        Log.d(TAG, "createAccount:" + email);
-        if (!validateForm()) {
-            return;
-        }
+            @Override
+            public void onClick(View v) {
+                final String userName = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
 
-        showProgressDialog();
 
-        // [START create_user_with_email]
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                final DatabaseReference userChildRef = mDatabase.child("gpsnow");
+
+                userChildRef.child("users").child(userName).addListenerForSingleValueEvent(
+                        new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                                if (dataSnapshot.getValue() != null) {
+                                    // Get user value
+
+                                    userNameFromServer = (String) dataSnapshot.child("username").getValue();
+                                    passwordFromServer = (String) dataSnapshot.child("password").getValue();
+                                    nameFromServer = (String) dataSnapshot.child("name").getValue();
+                                    if (passwordFromServer.equals(password)) {
+                                        loginStatus = true;
+                                        Toast.makeText(MainActivity.this, "validation success.", Toast.LENGTH_SHORT).show();
+                                        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                                        criteria = new Criteria();
+                                        provider = locationManager.getBestProvider(criteria, false);
+
+                                        if (ActivityCompat.checkSelfPermission(getBaseContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getBaseContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                                            // TODO: Consider calling
+                                            //    ActivityCompat#requestPermissions
+                                            // here to request the missing permissions, and then overriding
+                                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                            //                                          int[] grantResults)
+                                            // to handle the case where the user grants the permission. See the documentation
+                                            // for ActivityCompat#requestPermissions for more details.
+                                            return;
+                                        }
+                                        location = locationManager.getLastKnownLocation(provider);
+                                        double latitude = location.getLatitude();
+                                        stringLatitude = String.valueOf(latitude);
+                                        double longitude = location.getLongitude();
+                                        stringLongitude = String.valueOf(longitude);
+                                        HashMap<String, Object> result = new HashMap<>();
+                                        result.put("username", userName);
+                                        result.put("name",nameFromServer);
+                                        result.put("status",loginStatus);
+                                        result.put("Latitude",stringLatitude);
+                                        result.put("Longitude",stringLongitude);
+                                        userChildRef.child("login").child(userName).setValue(result);
+
+
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "Password incorrect", Toast.LENGTH_SHORT).show();
+                                    }
+
+
+                                } else {
+                                    Toast.makeText(MainActivity.this, "your user name is not registered.", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+
+
+                            // ...
+
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+                                Log.w(TAG, "getUser:onCancelled", databaseError.toException());
+                            }
+                        });
+                /*Query myTopPostsQuery = DatabaseReference.child("user-posts").child(myUserId)
+                        .orderByChild("starCount");
+
+                userChildRef.addListenerForSingleValueEvent(new ChildEventListener() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+                    private void onDataChange(DataSnapshot dataSnapshot) {
+                        Log.d("Value", "" + dataSnapshot.getValue());
+                        if (dataSnapshot.child(email).exists()) {
+                            DataSnapshot userSnapshot = (DataSnapshot) dataSnapshot.getChildren().iterator().next();
 
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+                            emailFromServer = (String) userSnapshot.child("email").getValue();
+                            passwordFromServer = (String) userSnapshot.child("password").getValue();
+
+                            if (password.equals(passwordFromServer)) {
+                                Toast.makeText(MainActivity.this, "Login success", Toast.LENGTH_SHORT).show();
+                            }
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "your email is not registered.", Toast.LENGTH_SHORT).show();
                         }
 
-                        // [START_EXCLUDE]
-                        hideProgressDialog();
-                        // [END_EXCLUDE]
                     }
-                });
-        // [END create_user_with_email]
-    }
 
-    private void signIn(String email, String password) {
-        Log.d(TAG, "signIn:" + email);
-        if (!validateForm()) {
-            return;
-        }
-
-        showProgressDialog();
-
-        // [START sign_in_with_email]
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
 
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "signInWithEmail", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-
-                        // [START_EXCLUDE]
-                        hideProgressDialog();
-                        // [END_EXCLUDE]
                     }
-                });
-        // [END sign_in_with_email]
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });*/
+
+
+            }
+
+        });
     }
 
-
-
-    private boolean validateForm() {
-        boolean valid = true;
-
-        String email = mEmailField.getText().toString();
-        if (TextUtils.isEmpty(email)) {
-            mEmailField.setError("Required.");
-            valid = false;
-        } else {
-            mEmailField.setError(null);
-        }
-
-        String password = mPasswordField.getText().toString();
-        if (TextUtils.isEmpty(password)) {
-            mPasswordField.setError("Required.");
-            valid = false;
-        } else {
-            mPasswordField.setError(null);
-        }
-
-        return valid;
-    }
-
-    private void updateUI(FirebaseUser user) {
-        hideProgressDialog();
-        if (user != null) {
-            mStatusTextView.setText(getString(R.string.emailpassword_status_fmt, user.getEmail()));
-            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
-
-            findViewById(R.id.email_password_buttons).setVisibility(View.GONE);
-            findViewById(R.id.email_password_fields).setVisibility(View.GONE);
-        } else {
-            mStatusTextView.setText(R.string.signed_out);
-            mDetailTextView.setText(null);
-
-            findViewById(R.id.email_password_buttons).setVisibility(View.VISIBLE);
-            findViewById(R.id.email_password_fields).setVisibility(View.VISIBLE);
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.email_create_account_button:
-                createAccount(mEmailField.getText().toString(), mPasswordField.getText().toString());
-                break;
-            case R.id.email_sign_in_button:
-                signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
-                break;
-
-        }
-    }
 }
--- app/src/main/java/com/surroundsync/gpsnow/sign_in.java
+++
@@ -1,7 +0,0 @@
-package com.surroundsync.gpsnow;
-
-/**
- * Created by Ashray Joshi on 08-Jun-16.
- */
-public class sign_in {
-}
